ed316f2a3ef8e412080bc6ee39a3f30491e3f77b
Mochammad Ridwan <moch.ridwan89@gmail.com>
2017-08-16 13:39:40 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;">
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Plant Controlling System</title>
+<link href="./administrator/config/style/login_style.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+<center><h2>Plant Controlling System</h2></center>
+<div class="loginform">
+<form method="post" action="administrator/config/check_user.php">
+<table width="100%" border="0" cellpadding="2">
+  <tr>
+    <td align="center"><b>User Name</b></td>
+  </tr>
+  <tr>
+    <td align="center"><input type="text" name="txtusername" value="" requred="required" class="txtlogin"/></td>
+  </tr>
+  <tr>
+    <td align="center"><b>Password</b></td>
+  </tr>
+  <tr>
+    <td align="center"><input type="password" name="txtpassword" value="" requred="required"  class="txtpassword"/></td>
+  </tr>
+  <tr>
+    <td align="center"><input type="submit" name="btnlogin" value="Login" class="btnlogin" /></td>
+  </tr>
+</table>
+</form>
+</div>
+<div class="footer"><center>Magkna &copy; 2017 All Right Reserved</center></div>
+</body>
+</html>
